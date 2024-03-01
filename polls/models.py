a5c104ca608e6a0f20e6345f050504453fab326f
wuyasan <youtosan0@gmail.com>
2024-02-29 21:13:03 -0500

    black formate

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
